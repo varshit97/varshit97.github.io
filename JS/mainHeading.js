5450c3b0bb4fe9d6b446a56d8fbdf69658a967ad
Varshit <varshit97@gmail.com>
2016-05-13 17:57:08 +0530

    Minor changes

--- JS/mainHeading.js
+++ JS/mainHeading.js
@@ -2,7 +2,7 @@ function animateHeading(jQuery){
   console.log('called');
   var id = '.wrapper';
   $(id).slideDown(3000);
-  $('div.inside').html("Varshit Battu").slideDown();
+  $('div.inside').html("varshit battu").slideDown();
 }
 
 $(document).ready(animateHeading);
