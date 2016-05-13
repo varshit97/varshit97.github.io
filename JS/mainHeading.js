f647711493809ffdeddcb2e6994afab17c31a62e
varshit97 <varshit97@gmail.com>
2016-05-14 01:06:16 +0530

    Add smiley recognition library

--- JS/mainHeading.js
+++ JS/mainHeading.js
@@ -1,8 +1,9 @@
 function animateHeading(jQuery){
-  console.log('called');
   var id = '.wrapper';
   $(id).slideDown(3000);
   $('div.inside').html("varshit battu").slideDown();
 }
 
 $(document).ready(animateHeading);
+
+$('.emoticon').emoticonize();
