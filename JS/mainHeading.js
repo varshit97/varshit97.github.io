df9d24a9084d4ebcc289ebf77739ca205ed8f076
varshit97 <varshit97@gmail.com>
2016-05-26 10:47:03 +0530

    Fix heading position for all zooms

--- JS/mainHeading.js
+++ JS/mainHeading.js
@@ -3,6 +3,13 @@ function animateHeading(jQuery){
   $(id).effect("bounce", { times:5 }, 3000);
 }
 
+var marginLeft = $('.wrapper').css('margin-left');
+marginLeft = marginLeft.replace('px','');
+var width = $('.wrapper').css('width');
+width = width.replace('px','');
+var newLeft = parseInt(marginLeft) - (parseInt(width)/2);
+$('.wrapper').css('margin-left', newLeft+'px');
+
 $(document).ready(animateHeading);
 
 $('.emoticon').emoticonize();
