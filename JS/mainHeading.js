13d4014c950aa8b779bda5017624d3aa9459fa5e
varshit97 <varshit97@gmail.com>
2016-05-26 11:10:18 +0530

    Fix main div position

--- JS/mainHeading.js
+++ JS/mainHeading.js
@@ -10,6 +10,14 @@ width = width.replace('px','');
 var newLeft = parseInt(marginLeft) - (parseInt(width)/2);
 $('.wrapper').css('margin-left', newLeft+'px');
 
+//For the mainDiv
+var left = $('.stitched').css('left');
+left = left.replace('px','');
+var width = $('.stitched').css('width');
+width = width.replace('px','');
+var newLeft = parseInt(left) - (parseInt(width)/2);
+$('.stitched').css('left', newLeft+'px');
+
 $(document).ready(animateHeading);
 
 $('.emoticon').emoticonize();
