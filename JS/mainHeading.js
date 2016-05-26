c7f6450097ca4aa05cee7ade2adcfe2a71a1d07c
varshit97 <varshit97@gmail.com>
2016-05-26 17:29:38 +0530

    Fix image position

--- JS/mainHeading.js
+++ JS/mainHeading.js
@@ -8,7 +8,7 @@ marginLeft = marginLeft.replace('px','');
 var width = $('.wrapper').css('width');
 width = width.replace('px','');
 var newLeft = parseInt(marginLeft) - (parseInt(width)/2);
-$('.wrapper').css('margin-left', newLeft+'px');
+$('.wrapper').css('margin-left', newLeft + 'px');
 
 //For the mainDiv
 var left = $('.stitched').css('left');
@@ -16,7 +16,22 @@ left = left.replace('px','');
 var width = $('.stitched').css('width');
 width = width.replace('px','');
 var newLeft = parseInt(left) - (parseInt(width)/2);
-$('.stitched').css('left', newLeft+'px');
+$('.stitched').css('left', newLeft + 'px');
+
+//For the mainImage
+var marginLeft1 = $('.box').css('margin-left');
+marginLeft1 = marginLeft1.replace('px','');
+var width = $('.box').css('width');
+width = width.replace('px','');
+var newLeft1 = parseInt(marginLeft1) - (parseInt(width)/2);
+$('.box').css('margin-left', newLeft1 + 'px');
+
+var bottom = $('.box').css('bottom');
+bottom = bottom.replace('px','');
+var height = $('.box').css('width');
+height = height.replace('px','');
+var newBottom = parseInt(bottom) - (parseInt(height)/2);
+$('.box').css('bottom', newBottom + 'px');
 
 $(document).ready(animateHeading);
 
